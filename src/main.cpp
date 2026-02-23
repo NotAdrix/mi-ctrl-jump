5937b4628d920a07c0c026444b71a9e7a0183019
NotAdrix <46830172+NotAdrix@users.noreply.github.com>
2026-02-23 13:20:55 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,22 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
+#include <Geode/utils/Keyboard.hpp>
+#include <Geode/loader/Event.hpp>
 
 using namespace geode::prelude;
 
-// Hook CCKeyboardDispatcher to intercept key events at the lowest level
 class $modify(CCKeyboardDispatcher) {
     bool dispatchKeyboardMSG(enumKeyCodes key, bool down, bool isRepeat, double unk) {
         auto mod = Mod::get();
+        auto keybind1 = mod->getSettingValue<geode::Keybind>("jump-keybind");
+        auto keybind2 = mod->getSettingValue<geode::Keybind>("jump-keybind-2");
 
-        // Read configured keybinds (stored as int64_t, cast to enumKeyCodes)
-        auto key1 = static_cast<enumKeyCodes>(mod->getSettingValue<int64_t>("jump-keybind"));
-        auto key2 = static_cast<enumKeyCodes>(mod->getSettingValue<int64_t>("jump-keybind-2"));
-
-        // If the pressed key matches either configured keybind, remap to Space
-        if (key == key1 || key == key2) {
-            return CCKeyboardDispatcher::dispatchKeyboardMSG(enumKeyCodes::KEY_Space, down, isRepeat, unk);
+        if (key == keybind1.key || key == keybind2.key) {
+            return CCKeyboardDispatcher::dispatchKeyboardMSG(
+                enumKeyCodes::KEY_Space, down, isRepeat, unk
+            );
         }
-
-        // Otherwise pass through normally
         return CCKeyboardDispatcher::dispatchKeyboardMSG(key, down, isRepeat, unk);
-    }
-};
-
-#include <Geode/modify/CCMouseDispatcher.hpp>
-
-class $modify(CCMouseDispatcher) {
-    bool dispatchScrollMSG(float x, float y) {
-        return CCMouseDispatcher::dispatchScrollMSG(x, y);
     }
 };
 
@@ -43,7 +33,6 @@ $execute {
             kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Z, down, false, 0.0);
             return ListenerResult::Stop;
         }
-
         if (data.button == geode::MouseInputData::Button::Middle) {
             kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_X, down, false, 0.0);
             return ListenerResult::Stop;
