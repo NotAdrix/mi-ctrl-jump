50db835008a79054c7761df419b2f360abaa56e7
NotAdrix <tu-correo@ejemplo.com>
2026-02-22 22:29:16 -0300

    build2

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,26 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/UILayer.hpp>
+#include <Geode/modify/CCKeyboardDispatcher.hpp>
 
 using namespace geode::prelude;
 
-class $modify(UILayer) {
-    void keyDown(enumKeyCodes key, double timestamp) {
+class $modify(CCKeyboardDispatcher) {
+    bool dispatchKeyboardMSG(enumKeyCodes key, bool isKeyDown, bool isKeyRepeat) {
+        // Si es la tecla Control
         if (key == enumKeyCodes::KEY_Control) {
             auto playLayer = PlayLayer::get();
+            
+            // Solo actuar si estamos jugando y el puntero al jugador es válido
             if (playLayer && playLayer->m_player1) {
-                // En la 2.2081 se usa m_player1 para saltar
-                playLayer->m_player1->pushButton(PlayerButton::Jump);
+                if (isKeyDown) {
+                    playLayer->m_player1->pushButton(PlayerButton::Jump);
+                } else {
+                    playLayer->m_player1->releaseButton(PlayerButton::Jump);
+                }
+                // Retornamos true para que el juego ignore la tecla nativa
+                return true;
             }
         }
-        UILayer::keyDown(key, timestamp);
-    }
-
-    void keyUp(enumKeyCodes key, double timestamp) {
-        if (key == enumKeyCodes::KEY_Control) {
-            auto playLayer = PlayLayer::get();
-            if (playLayer && playLayer->m_player1) {
-                // Para soltar el salto
-                playLayer->m_player1->releaseButton(PlayerButton::Jump);
-            }
-        }
-        UILayer::keyUp(key, timestamp);
+        // Para todo lo demás, comportamiento normal
+        return CCKeyboardDispatcher::dispatchKeyboardMSG(key, isKeyDown, isKeyRepeat);
     }
 };
