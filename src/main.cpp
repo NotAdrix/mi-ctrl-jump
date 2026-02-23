657eb79ac56ea24e4698ab31f16873bc4189169c
NotAdrix <tu-correo@ejemplo.com>
2026-02-23 05:08:14 -0300

    dios

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
-geode::KeyboardInputEvent().listen(+[](geode::KeyboardInputData& data) {
-    log::debug("key={} action={} mods={}", 
-        (int)data.key, 
-        (int)data.action, 
-        (int)data.modifiers.value
-    );
-    return ListenerResult::Propagate;
-}).leak();
+#include <Geode/Geode.hpp>
+#include <Geode/utils/Keyboard.hpp>
+#include <Geode/loader/Event.hpp>
+
+using namespace geode::prelude;
+
+$execute {
+    geode::KeyboardInputEvent().listen(+[](geode::KeyboardInputData& data) {
+        log::debug("key={} action={} mods={}", 
+            (int)data.key, 
+            (int)data.action, 
+            (int)data.modifiers.value
+        );
+        return ListenerResult::Propagate;
+    }).leak();
+}
