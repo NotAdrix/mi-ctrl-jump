bd0738d15f7676bc6dd5a599f22f5e2360c9310f
NotAdrix <tu-correo@ejemplo.com>
2026-02-23 03:28:01 -0300

    dios

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CCScheduler.hpp>
 #include <Geode/modify/CCKeyboardDispatcher.hpp>
-#include <Geode/modify/PlayerObject.hpp>
+#include <Geode/binding/PlayLayer.hpp>
+#include <Geode/binding/PlayerObject.hpp>
 
 #ifdef GEODE_IS_WINDOWS
 #include <Windows.h>
@@ -9,112 +10,50 @@
 
 using namespace geode::prelude;
 
-// Variables de estado para evitar spam de teclas
-static bool g_ctrlDown = false;
-static bool g_rightDown = false;
-static bool g_middleDown = false;
+// Variables de estado únicas (evita el error de redefinición)
+static bool g_ctrlDownState = false;
+static bool g_rightDownState = false;
+static bool g_middleDownState = false;
 
-/**
- * REASIGNACIÓN GLOBAL Y DE GAMEPLAY
- * Esta versión lee el hardware (infalible) y ejecuta el salto directamente
- * en el objeto del jugador (compatibilidad total con 2.2 y CBF).
- */
 class $modify(CCScheduler) {
     void update(float dt) {
         CCScheduler::update(dt);
 
 #ifdef GEODE_IS_WINDOWS
-        // Obtenemos el estado de la partida actual
-        auto playLayer = PlayLayer::get();
         auto kbd = CCKeyboardDispatcher::get();
+        auto playLayer = PlayLayer::get();
 
-        // 1. DETECCIÓN DE CONTROL (Salto / Espacio)
+        // 1. SOLUCIÓN PARA CTRL (SALTO DIRECTO)
         bool currentCtrl = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
-        if (currentCtrl != g_ctrlDown) {
-            g_ctrlDown = currentCtrl;
+        if (currentCtrl != g_ctrlDownState) {
+            g_ctrlDownState = currentCtrl;
             
+            // Si estamos jugando, le ordenamos al objeto físico saltar
             if (playLayer && playLayer->m_player1) {
-                // Si estamos jugando, forzamos el salto directo (Precisión Máxima)
-                if (g_ctrlDown) playLayer->m_player1->pushButton(PlayerButton::Jump);
-                else playLayer->m_player1->releaseButton(PlayerButton::Jump);
-            } else if (kbd) {
-                // Si estamos en un menú, mandamos un Espacio normal
-                kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Space, g_ctrlDown, false, 0.0);
+                if (g_ctrlDownState) {
+                    playLayer->m_player1->pushButton(PlayerButton::Jump);
+                } else {
+                    playLayer->m_player1->releaseButton(PlayerButton::Jump);
+                }
+            } 
+            // En menús, mandamos la tecla Space normal
+            if (kbd) {
+                kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Space, g_ctrlDownState, false, 0.0);
             }
         }
 
-        // 2. DETECCIÓN DE CLICK DERECHO (Tecla Z)
+        // 2. SOLUCIÓN PARA CLICK DERECHO (Z)
         bool currentRight = (GetAsyncKeyState(VK_RBUTTON) & 0x8000) != 0;
-        if (currentRight != g_rightDown) {
-            g_rightDown = currentRight;
-            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Z, g_rightDown, false, 0.0);
+        if (currentRight != g_rightDownState) {
+            g_rightDownState = currentRight;
+            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Z, g_rightDownState, false, 0.0);
         }
 
-        // 3. DETECCIÓN DE RUEDITA (Tecla X)
+        // 3. SOLUCIÓN PARA RUEDITA (X)
         bool currentMiddle = (GetAsyncKeyState(VK_MBUTTON) & 0x8000) != 0;
-        if (currentMiddle != g_middleDown) {
-            g_middleDown = currentMiddle;
-            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_X, g_middleDown, false, 0.0);
-        }
-#endif
-    }
-};#include <Geode/Geode.hpp>
-#include <Geode/modify/CCScheduler.hpp>
-#include <Geode/modify/CCKeyboardDispatcher.hpp>
-#include <Geode/modify/PlayerObject.hpp>
-
-#ifdef GEODE_IS_WINDOWS
-#include <Windows.h>
-#endif
-
-using namespace geode::prelude;
-
-// Variables de estado para evitar spam de teclas
-static bool g_ctrlDown = false;
-static bool g_rightDown = false;
-static bool g_middleDown = false;
-
-/**
- * REASIGNACIÓN GLOBAL Y DE GAMEPLAY
- * Esta versión lee el hardware (infalible) y ejecuta el salto directamente
- * en el objeto del jugador (compatibilidad total con 2.2 y CBF).
- */
-class $modify(CCScheduler) {
-    void update(float dt) {
-        CCScheduler::update(dt);
-
-#ifdef GEODE_IS_WINDOWS
-        // Obtenemos el estado de la partida actual
-        auto playLayer = PlayLayer::get();
-        auto kbd = CCKeyboardDispatcher::get();
-
-        // 1. DETECCIÓN DE CONTROL (Salto / Espacio)
-        bool currentCtrl = (GetAsyncKeyState(VK_CONTROL) & 0x8000) != 0;
-        if (currentCtrl != g_ctrlDown) {
-            g_ctrlDown = currentCtrl;
-            
-            if (playLayer && playLayer->m_player1) {
-                // Si estamos jugando, forzamos el salto directo (Precisión Máxima)
-                if (g_ctrlDown) playLayer->m_player1->pushButton(PlayerButton::Jump);
-                else playLayer->m_player1->releaseButton(PlayerButton::Jump);
-            } else if (kbd) {
-                // Si estamos en un menú, mandamos un Espacio normal
-                kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Space, g_ctrlDown, false, 0.0);
-            }
-        }
-
-        // 2. DETECCIÓN DE CLICK DERECHO (Tecla Z)
-        bool currentRight = (GetAsyncKeyState(VK_RBUTTON) & 0x8000) != 0;
-        if (currentRight != g_rightDown) {
-            g_rightDown = currentRight;
-            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_Z, g_rightDown, false, 0.0);
-        }
-
-        // 3. DETECCIÓN DE RUEDITA (Tecla X)
-        bool currentMiddle = (GetAsyncKeyState(VK_MBUTTON) & 0x8000) != 0;
-        if (currentMiddle != g_middleDown) {
-            g_middleDown = currentMiddle;
-            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_X, g_middleDown, false, 0.0);
+        if (currentMiddle != g_middleDownState) {
+            g_middleDownState = currentMiddle;
+            if (kbd) kbd->dispatchKeyboardMSG(enumKeyCodes::KEY_X, g_middleDownState, false, 0.0);
         }
 #endif
     }
